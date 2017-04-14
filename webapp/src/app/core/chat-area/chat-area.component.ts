ffc201e47331f9aa0cc4f2a6bc66c2d59cea542c
Kasidit Iamthong <kasidit@outlook.com>
2017-04-15 01:10:09 +0700

    Add socket test page

--- webapp/src/app/core/chat-area/chat-area.component.ts
+++ webapp/src/app/core/chat-area/chat-area.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { APISocketService } from '../../shared/base/api-socket.service';
+
+interface Message {
+  username: string;
+  message: string;
+}
 
 @Component({
   selector: 'app-chat-area',
@@ -6,10 +14,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-area.component.scss']
 })
 export class ChatAreaComponent implements OnInit {
+  chatMessages: Observable<Message[]>;
+  messageInput: string;
 
-  constructor() { }
+  constructor(private socket: APISocketService) { }
 
   ngOnInit() {
+    const messageListener = this.socket.getEvent<Message>('new message');
+    this.chatMessages = messageListener.map(msg => {
+      return [msg];
+    }).scan((acc, newMsg) => {
+      return [...acc, ...newMsg];
+    });
   }
 
+  sendMessage() {
+    if (this.messageInput === '') {
+      return;
+    }
+    this.socket.sendMessage('new message', this.messageInput);
+    this.messageInput = '';
+  }
 }
