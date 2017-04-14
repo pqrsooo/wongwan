4c91c175c192456cf58a593b9e288fca4a175bf9
Kasidit Iamthong <kasidit@outlook.com>
2017-04-15 01:38:18 +0700

    Revert "Add socket test page"
    This reverts commit ffc201e47331f9aa0cc4f2a6bc66c2d59cea542c.

--- webapp/src/app/core/chat-area/chat-area.component.ts
+++ webapp/src/app/core/chat-area/chat-area.component.ts
@@ -1,12 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-
-import { APISocketService } from '../../shared/base/api-socket.service';
-
-interface Message {
-  username: string;
-  message: string;
-}
 
 @Component({
   selector: 'app-chat-area',
@@ -14,25 +6,10 @@ interface Message {
   styleUrls: ['./chat-area.component.scss']
 })
 export class ChatAreaComponent implements OnInit {
-  chatMessages: Observable<Message[]>;
-  messageInput: string;
 
-  constructor(private socket: APISocketService) { }
+  constructor() { }
 
   ngOnInit() {
-    const messageListener = this.socket.getEvent<Message>('new message');
-    this.chatMessages = messageListener.map(msg => {
-      return [msg];
-    }).scan((acc, newMsg) => {
-      return [...acc, ...newMsg];
-    });
   }
 
-  sendMessage() {
-    if (this.messageInput === '') {
-      return;
-    }
-    this.socket.sendMessage('new message', this.messageInput);
-    this.messageInput = '';
-  }
 }
