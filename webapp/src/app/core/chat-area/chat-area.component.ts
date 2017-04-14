b9413f6f02620c6829b4db62232be0181bd9ed85
Parinthorn Saithong <parinthorn.sa@gmail.com>
2017-04-15 02:24:26 +0700

    Add send button and scroll conversation container to bottom after init

--- webapp/src/app/core/chat-area/chat-area.component.ts
+++ webapp/src/app/core/chat-area/chat-area.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-chat-area',
   templateUrl: './chat-area.component.html',
   styleUrls: ['./chat-area.component.scss']
 })
-export class ChatAreaComponent implements OnInit {
+export class ChatAreaComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('conversationContainer') conversationContainer: ElementRef;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    this.scrollToBottom();
   }
 
+  scrollToBottom() {
+    this.conversationContainer.nativeElement.scrollTop = this.conversationContainer.nativeElement.scrollHeight;
+  }
 }
