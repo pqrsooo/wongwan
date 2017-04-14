4e26f7ffdb61c69e2359c999253819189c70219e
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-04-14 17:50:36 +0700

    Add socket.io basic server function

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const config = require('./config');
 const app = require('./app');
 const db = require('./db');
+const ioServer = require('socket.io');
 
 db.setUpDatabase().then(() => {
   app.listen(config.express.port, config.express.ip, (err) => {
@@ -13,4 +14,62 @@ db.setUpDatabase().then(() => {
 }).catch((err) => {
   console.error('Unable to connect to database', err);
   process.exit(10);
+});
+
+const io = ioServer(app.server);
+const chat = io.of('/chat');
+
+let clientListNames = [];
+let numUsers = 0;
+
+io.on('connection', (socket) => {
+  let addedUser = false;
+
+  socket.on('new message', (data) => {
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    })
+  })
+
+  socket.on('add user', (username) => {
+    if(addedUser) return;
+
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    })
+  })
+
+  socket.on('disconnect', () => {
+    if(addedUser) {
+      --numUsers;
+
+      socket.broadcast.emit('user left',{
+        username: socket.username,
+        numUsers
+      });
+    }
+  });
+
 });
