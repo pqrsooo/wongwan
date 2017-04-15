fa7e85b0198f04ef6132335475e03ca00fc6fc34
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-04-15 19:24:44 +0700

    Modify User Model

--- api/user/user.model.js
+++ api/user/user.model.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
+    username: { type: String, required: true, index: { unique: true } },
+    password: { type: String, required: true },
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true}
 });
 //Users 
 module.exports = mongoose.model('User',userSchema);
