cb193b58b557ad756320af73939e7513015def43
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-04-15 19:23:43 +0700

    Add body-parser as middleware

--- api/app.js
+++ api/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get('/api/ping', (req, res) => {
-  res.json({ message: 'pong' });
-});
-
-app.use('/api/user', require('./user/user.route'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api/user', require('./user/user.route'));
 
 module.exports = app;
