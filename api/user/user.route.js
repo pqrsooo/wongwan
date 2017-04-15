21c6c7b0b1d25501eda1fee85e7cc6cdd27ded6a
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-04-15 19:25:22 +0700

    Add user api for login and signup

--- api/user/user.route.js
+++ api/user/user.route.js
@@ -1,11 +1,82 @@
 const express = require('express');
 const User = require('./user.model');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const config = require('../config');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/signup', (req, res) => {
 
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send({
+      error: 'No username or password defined'
+    });
+  }
+
+  const saltRounds = 10;
+  bcrypt.hash(req.body.password, saltRounds).then(function (hash) {
+    const newUser = new User({
+      username: req.body.username,
+      password: hash,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName
+    })
+    newUser.save().then((user) => {
+      console.log('Successfully create new user')
+      res.status(200).send({
+        message: 'success',
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName
+      });
+    }).catch((err) => {
+      if (err.code === 11000) {
+        res.status(400).send({
+          error: 'This username already exists'
+        });
+      } else {
+        res.status(400).send({
+          error: 'Cannot create new user'
+        })
+      }
+    });
+  });
+});
+
+router.post('/login', (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send({
+      error: 'No username or password defined'
+    });
+  };
+  User.findOne({
+    username: req.body.username
+  }).then((user) => {
+    bcrypt.compare(req.body.password, user.password).then((result) => {
+      if(result) {
+        res.status(200).json({
+          message : 'Success',
+          user : user
+        });
+      } else {
+        res.status(400).json({
+          error: 'Invalid Password'
+        })
+      }
+    }).catch(err=>{
+      console.error(err);
+      res.status(500).send({
+        error: 'Internal Error'
+      })
+    })
+  }).catch((err) => {
+    console.error(err);
+    res.status(400).send({
+      error: 'Could not find user'
+    })
+  });
 });
 
 router.get('/:username', (req, res) => {
