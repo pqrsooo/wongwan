ba3b0b60f38732f94279708c3415b484eba61664
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-04-16 12:06:51 +0700

    Modify model to use ObjectID

--- api/chatroom/chatroom.model.js
+++ api/chatroom/chatroom.model.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-const objID = mongoose.Schema.Types.ObjectId;
+const ObjectID = mongoose.Schema.Types.ObjectId;
 
 const chatRoomSchema = new mongoose.Schema({
     roomName: String,
     roomID: String,
     users: [{
-        username: String,
-        lastSeenMessage: objID,
+        userID: ObjectID,
     }],
 });
 const m = mongoose.model('chatRoom',chatRoomSchema);
--- api/user/user.model.js
+++ api/user/user.model.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true },
     firstName: {type: String, required: true},
-    lastName: {type: String, required: true}
+    lastName: {type: String, required: true},
+    chatRooms : [{
+        roomID : ObjectId,
+        lastSeenMeesage: ObjectId
+    }]
 });
 //Users 
 module.exports = mongoose.model('User',userSchema);
