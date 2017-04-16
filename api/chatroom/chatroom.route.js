c81d798a5aeba489977d4f06eed25f2f441edb91
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-04-16 16:02:29 +0700

    Add chatroom create-room api

--- api/chatroom/chatroom.route.js
+++ api/chatroom/chatroom.route.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('../config');
 const Chatroom = require('./chatroom.model');
+const chatRoomQuery = require('./chatroom.query');
+const userQuery = require('../user/user.query');
+const utils = require('./utilities');
 const router = express.Router();
 
 //TODO - Add unauthorized middleware
@@ -10,14 +13,36 @@ const router = express.Router();
 router.post('/create-room', (req, res) => {
   const session = req.session;
   const user = session.user;
-  const users = req.body.users;
+  // will seach for objectID from username array
+  // join Users should be array of User object
 
+  const users = query.getAllUserID(req.body.joinUsers);
+  const roomToken = utils.randomToken(48);
   const chatRoom = new Chatroom({
-    room:,
-    token:,
-    users: users
+    roomName: req.body.roomName,
+    roomToken: roomToken,
   });
-
+  chatRoomQuery.saveChatRoom(chatRoom).then(data => {
+    userQuery.addChatRoom(users, chatRoom).then(data => {
+      res.status(200).json({
+        success: true,
+        message: 'Successfully create Chatroom',
+        roomToken: chatRoom.roomToken,
+      });
+    }).catch(err => {
+      console.error('Fail to addChatroom with error', err);
+      res.status(400).json({
+        success: false,
+        message: 'Fail to create Chatroom',
+      });
+    });
+  }).catch(err => {
+    console.error('Fail to createChatroom', err);
+    res.status(400).json({
+      success: false,
+      message: 'Fail to create Chatroom',
+    });
+  });
 });
 
 // for getting all the room for particular user
@@ -26,8 +51,6 @@ router.get('/get-chatroom', (req, res) => {
   const user = session.user;
 
 
-
 });
-
 
 module.exports = router;
