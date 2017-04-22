ae289bd661381b39ec02964d457b25f5d62a17a4
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-04-23 06:54:25 +0700

    Move socket.io from index.js to separated file

--- api/app.js
+++ api/app.js
@@ -5,8 +5,10 @@ const redis = require('redis');
 const RedisStore = require('connect-redis')(session);
 const config = require('./config');
 const unauthorizedFunc = require('./middleware/unauthorized')
+const http = require('http');
 
 const app = express();
+const appServer = http.Server(app);
 const client = redis.createClient('6379', 'redis');
 
 // Use redis store as the store of express-session
@@ -26,6 +28,7 @@ app.use(session({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Router
 app.get('/api/restricted', unauthorizedFunc.restrict, (req, res) => {
   res.status(200).json({
     isLogin: true,
@@ -37,4 +40,8 @@ app.get('/api/restricted', unauthorizedFunc.restrict, (req, res) => {
 app.use('/api/user', require('./user/user.route'));
 app.use('/api/chatroom', require('./chatroom/chatroom.route'));
 
-module.exports = app;
+module.exports = {
+  app,
+  appServer,
+};
+
--- api/index.js
+++ api/index.js
@@ -1,13 +1,7 @@
 const config = require('./config');
-const app = require('./app');
+const appServer = require('./app').appServer;
 const db = require('./db');
-const ioServer = require('socket.io');
-const http = require('http');
-const ChatRoom = require('./chatroom/chatroom.model');
-const User = require('./user/user.model');
-const Message = require('./message/message.model');
-
-const appServer = http.Server(app);
+const createSocketServer = require('./socket/socket.base');
 
 db.setUpDatabase().then(() => {
   appServer.listen(config.express.port, config.express.ip, (err) => {
@@ -22,73 +16,8 @@ db.setUpDatabase().then(() => {
   process.exit(10);
 });
 
-const io = ioServer(appServer, { serveClient:false });
-const chat = io.of('/chat');
-
-let clientListNames = [];
-let numUsers = 0;
+const io = createSocketServer(appServer);
 
 io.on('connection', (socket) => {
-  let addedUser = false;
-  // emit 'new message' when user type in
-  socket.on('new message', (data) => {
-    const message = {
-      username: socket.username,
-      content: data,
-      id: Date.now().toString() // TODO: Change to Mongo's _id
-    };
-
-    // We use setTimeout to simulate latency and use randomness to simulate out-of-order.
-    setTimeout(() => {
-      socket.broadcast.emit('new message', message)
-    }, Math.round(Math.random() * 3000));
-
-    setTimeout(() => {
-      socket.emit('new message ack', {
-        success: true,
-        message: message
-      });
-    }, Math.round(Math.random() * 500) + 200);
-  })
-
-  socket.on('add user', (username) => {
-    if(addedUser) return;
-
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
-  socket.on('typing', () => {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    })
-  })
-
-  socket.on('disconnect', () => {
-    if(addedUser) {
-      --numUsers;
-
-      socket.broadcast.emit('user left',{
-        username: socket.username,
-        numUsers
-      });
-    }
-  });
-
+  //just call to make io connect
 });
