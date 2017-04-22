9e5f95b4a9a420366e13d32807814edbbce1111f
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-04-23 06:58:02 +0700

    Fix Eslint stuff

--- api/message/message.model.js
+++ api/message/message.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 const objectID = mongoose.Schema.Types.ObjectId;
 
 const chatMessageSchema = new mongoose.Schema({
--- api/message/message.query.js
+++ api/message/message.query.js
@@ -59,4 +59,6 @@ module.exports = {
   saveMessage,
   getMessageFromRoom,
   getLatestMessageInRoom,
-}
+};
+
+
