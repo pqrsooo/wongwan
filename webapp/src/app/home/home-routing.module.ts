438777a23cdcb29428812748a7494783ff57b022
Kasidit Iamthong <kasidit@outlook.com>
2017-04-26 03:30:05 +0700

    Make placeholder for JoinRoomComponent

--- webapp/src/app/home/home-routing.module.ts
+++ webapp/src/app/home/home-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CreateRoomComponent } from './create-room/create-room.component';
 import { HomeComponent } from './home.component';
+import { JoinRoomComponent } from './join-room/join-room.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'create-room', component: CreateRoomComponent }
+  { path: 'create-room', component: CreateRoomComponent },
+  { path: 'join-room', component: JoinRoomComponent }
 ];
 
 @NgModule({
--- webapp/src/app/home/home.module.ts
+++ webapp/src/app/home/home.module.ts
@@ -6,11 +6,12 @@ import { CreateRoomComponent } from './create-room/create-room.component';
 import { CreateRoomService } from './create-room/create-room.service';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { JoinRoomComponent } from './join-room/join-room.component';
 
 @NgModule({
   imports: [HomeRoutingModule, SharedModule, CoreModule],
   exports: [],
-  declarations: [HomeComponent, CreateRoomComponent],
+  declarations: [HomeComponent, CreateRoomComponent, JoinRoomComponent],
   providers: [CreateRoomService],
 })
 export class HomeModule { }
