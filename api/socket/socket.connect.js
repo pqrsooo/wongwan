0a5967268011023c7e67d1dfba7fd59f395a815f
Kasidit Iamthong <kasidit@outlook.com>
2017-04-26 03:49:46 +0700

    Implements joinRoom logic

--- api/socket/socket.connect.js
+++ api/socket/socket.connect.js
@@ -24,204 +24,205 @@ function onConnect(socket) {
         socket.join(roomToken);
       });
     });
-  });
 
-  /**
-   * 'subscribe event' let the server join the client
-   *  to the rooms(Tokens) sent from the client
-   *
-   *  @param {Array} rooms
-   */
-  // socket.on('subscribe', (rooms) => {
-  //   rooms.forEach((room) => {
-  //     console.log(`subscribe client ${socket.id} to ${room}`);
-  //     socket.join(room);
-  //   });
-  // });
-  /**
-   * 'unsubscribe event' let the server disconnect the client from
-   *  specific rooms when user leave the room
-   */
-  socket.on('unsubscribe', (rooms) => {
-    rooms.forEach((room) => {
-      console.log(`unsubscribe client ${socket.id} from ${room}`);
-      socket.leave(room);
-    });
-  });
-  /**
-   *  The 'send' notify the server to save the message and then
-   *  broadcast to all user in the same room with event 'new message'
-   *  and send acknowledge with event 'new message ack'
-   *  back to the client(sender) as well
-   */
-  socket.on('send', (data) => {
-    const messagePrefab = {
-      sender: {},
-    }; // message object must follow the Message Schema
-    messagePrefab.content = data.content;
-    userQuery.getUserFromUsername(data.username).then((user) => {
-      messagePrefab.sender.id = user.id;
-      messagePrefab.sender.firstName = user.firstName;
-      messagePrefab.sender.lastName = user.lastName;
-      messagePrefab.sender.username = user.username;
-      return chatRoomQuery.getChatroomID(data.room);
-    }).then((roomID) => {
-      messagePrefab.roomID = roomID;
-      const message = new Message(messagePrefab);
-      return message.save();
-    }).then((msg) => {
-      socket.in(data.room).emit('new message', {
-        content: msg.content,
-        createdTime: msg.createdAt,
-        messageID: msg.id,
-        room: data.room,
-        sender: msg.sender.firstName,
-        username: msg.sender.username,
+    /**
+     * 'subscribe event' let the server join the client
+     *  to the rooms(Tokens) sent from the client
+     *
+     *  @param {Array} rooms
+     */
+    // socket.on('subscribe', (rooms) => {
+    //   rooms.forEach((room) => {
+    //     console.log(`subscribe client ${socket.id} to ${room}`);
+    //     socket.join(room);
+    //   });
+    // });
+    /**
+     * 'unsubscribe event' let the server disconnect the client from
+     *  specific rooms when user leave the room
+     */
+    socket.on('unsubscribe', (rooms) => {
+      rooms.forEach((room) => {
+        console.log(`unsubscribe client ${socket.id} from ${room}`);
+        socket.leave(room);
       });
-      socket.emit('new message ack', {
-        success: true,
-        message: {
+    });
+    /**
+     *  The 'send' notify the server to save the message and then
+     *  broadcast to all user in the same room with event 'new message'
+     *  and send acknowledge with event 'new message ack'
+     *  back to the client(sender) as well
+     */
+    socket.on('send', (data) => {
+      const messagePrefab = {
+        sender: {},
+      }; // message object must follow the Message Schema
+      messagePrefab.content = data.content;
+      userQuery.getUserFromUsername(data.username).then((user) => {
+        messagePrefab.sender.id = user.id;
+        messagePrefab.sender.firstName = user.firstName;
+        messagePrefab.sender.lastName = user.lastName;
+        messagePrefab.sender.username = user.username;
+        return chatRoomQuery.getChatroomID(data.room);
+      }).then((roomID) => {
+        messagePrefab.roomID = roomID;
+        const message = new Message(messagePrefab);
+        return message.save();
+      }).then((msg) => {
+        socket.in(data.room).emit('new message', {
           content: msg.content,
           createdTime: msg.createdAt,
           messageID: msg.id,
           room: data.room,
           sender: msg.sender.firstName,
           username: msg.sender.username,
-        }
-      });
-    })
-      .catch((err) => {
-        console.error('Cannot Save Message ', err);
+        });
         socket.emit('new message ack', {
-          success: false,
-          message: data.message,
+          success: true,
+          message: {
+            content: msg.content,
+            createdTime: msg.createdAt,
+            messageID: msg.id,
+            room: data.room,
+            sender: msg.sender.firstName,
+            username: msg.sender.username,
+          }
+        });
+      })
+        .catch((err) => {
+          console.error('Cannot Save Message ', err);
+          socket.emit('new message ack', {
+            success: false,
+            message: data.message,
+            room: data.room,
+          });
+        });
+    });
+
+    /**
+     *  The 'typing event' tell the server to
+     *  broadcast to other clients that they are typing
+     *
+     *  @param {Object} data {roomToken, sender}
+     */
+    socket.on('typing', (data) => {
+      socket.to(data.roomToken).emit('typing', {
+        sender: data.sender,
+      });
+    });
+
+    /**
+     *  The 'stop typing event' tell the server to
+     *  broadcast to other clients that the user has already
+     *  stop typing
+     *
+     *  @param {Object} data {roomToken, sender}
+     */
+    socket.on('stop typing', (data) => {
+      socket.to(data.roomToken).emit('typing', {
+        sender: data.sender,
+      });
+    });
+    /**
+     *  The 'read' tell the server that all the messages
+     *  of a specific user in the room has been read
+     *  the server will then update the user lastSeenMessage
+     *
+     *  @param {Object} data {roomToken, user}
+     */
+    socket.on('read', (data) => {
+      let roomID = null;
+      const roomToken = data.roomToken;
+      const seenMsg = ObjectId(data.messageID);
+      chatRoomQuery.getChatroomID(roomToken).then((room) => {
+        roomID = room._id;
+        return userQuery.updateLastSeenMessageInRoom(data.user, roomID, seenMsg);
+      }).then((updatedUser) => {
+        socket.emit('read ack', {
+          success: true,
+          seenTimestamp: updatedUser.updatedAt,
+          seenMessageID: data.messageID,
           room: data.room,
         });
+        socket.to(data.roomToken).emit('seen', {
+          success: true,
+          seenTimestamp: updatedUser.updatedAt,
+          seenMessageID: seenMsg.id,
+          seenUsername: updatedUser.username,
+          seenUser: updatedUser.firstName,
+          room: data.room,
+        });
+      }).catch((err) => {
+        console.error('Cannot Set Read status ', err);
+        socket.emit('read ack', {
+          success: false,
+        });
       });
-  });
+    });
+    /**
+     *  The 'join room' event tell the server to
+     *  broadcast to other clients that the user has already
+     *  joined the group
+     *
+     *  @param {Object} data {roomToken}
+     */
+    socket.on('join room', (data) => {
+      const username = session.user.username;
+      const roomToken = data.roomToken;
+      chatRoomQuery.getChatroom(roomToken).then((room) => {
+        return userQuery.joinChatRoom(username, room._id);
+      }).then((updatedUser) => {
+        socket.to(roomToken).emit('new user join', {
+          success: true,
+          username: updatedUser.username,
+          firstName: updatedUser.firstName,
+          ts: updatedUser.updatedAt,
+        });
+        socket.emit('join room ack', {
+          success: true,
+          ts: updatedUser.updatedAt,
+        });
+        socket.join(roomToken);
+      }).catch((err) => {
+        console.error('Cannot join room', err);
+        socket.emit('join room ack', {
+          success: false,
+        });
+      });
+      socket.to(data.roomToken).emit('typing', {
+        sender: data.sender,
+      });
+    });
+    /**
+     *  The 'leave room' event tell the server to
+     *  broadcast to other clients that the user has permanently
+     *  leaved the group. Also, the server will remove the client
+     *  from the chatroom
+     *
+     *  @param {Object} data {roomToken, sender}
+     */
+    socket.on('leave room', (data) => {
+      const username = data.sender;
+      const roomToken = data.roomToken;
+      chatRoomQuery.getChatroom(roomToken).then((room) => {
+        return userQuery.leaveChatRoom(username, room._id);
+      }).then((updatedUser) => {
+        socket.to(roomToken).emit('user left', {
+          success: true,
+          username: updatedUser.username,
+          firstName: updatedUser.firstName,
+          ts: updatedUser.updatedAt,
+        });
+        socket.emit('leave room ack', {
+          success: true,
+          ts: updatedUser.updatedAt,
+        });
+        socket.leave(roomToken);
+      }).catch((err) => {
+        console.error('Error while leaving room', err);
+      });
+    });
 
-  /**
-   *  The 'typing event' tell the server to
-   *  broadcast to other clients that they are typing
-   *
-   *  @param {Object} data {roomToken, sender}
-   */
-  socket.on('typing', (data) => {
-    socket.to(data.roomToken).emit('typing', {
-      sender: data.sender,
-    });
-  });
-
-  /**
-   *  The 'stop typing event' tell the server to
-   *  broadcast to other clients that the user has already
-   *  stop typing
-   *
-   *  @param {Object} data {roomToken, sender}
-   */
-  socket.on('stop typing', (data) => {
-    socket.to(data.roomToken).emit('typing', {
-      sender: data.sender,
-    });
-  });
-  /**
-   *  The 'read' tell the server that all the messages
-   *  of a specific user in the room has been read
-   *  the server will then update the user lastSeenMessage
-   *
-   *  @param {Object} data {roomToken, user}
-   */
-  socket.on('read', (data) => {
-    let roomID = null;
-    const roomToken = data.roomToken;
-    const seenMsg = ObjectId(data.messageID);
-    chatRoomQuery.getChatroomID(roomToken).then((room) => {
-      roomID = room._id;
-      return userQuery.updateLastSeenMessageInRoom(data.user, roomID, seenMsg);
-    }).then((updatedUser) => {
-      socket.emit('read ack', {
-        success: true,
-        seenTimestamp: updatedUser.updatedAt,
-        seenMessageID: data.messageID,
-        room: data.room,
-      });
-      socket.to(data.roomToken).emit('seen', {
-        success: true,
-        seenTimestamp: updatedUser.updatedAt,
-        seenMessageID: seenMsg.id,
-        seenUsername: updatedUser.username,
-        seenUser: updatedUser.firstName,
-        room: data.room,
-      });
-    }).catch((err) => {
-      console.error('Cannot Set Read status ', err);
-      socket.emit('read ack', {
-        success: false,
-      });
-    });
-  });
-  /**
-   *  The 'join room' event tell the server to
-   *  broadcast to other clients that the user has already
-   *  joined the group
-   *
-   *  @param {Object} data {roomToken, sender}
-   */
-  socket.on('join room', (data) => {
-    const username = data.sender;
-    const roomToken = data.roomToken;
-    chatRoomQuery.getChatroom(roomToken).then((room) => {
-      return userQuery.joinChatRoom(username, room._id);
-    }).then((updatedUser) => {
-      socket.to(roomToken).emit('new user join', {
-        success: true,
-        username: updatedUser.username,
-        firstName: updatedUser.firstName,
-        ts: updatedUser.updatedAt,
-      });
-      socket.emit('join room ack', {
-        success: true,
-        ts: updatedUser.updatedAt,
-      });
-      socket.join(roomToken);
-    }).catch((err) => {
-      console.error('Cannot join room', err);
-      socket.emit('join room ack', {
-        success: false,
-      });
-    });
-    socket.to(data.roomToken).emit('typing', {
-      sender: data.sender,
-    });
-  });
-  /**
-   *  The 'leave room' event tell the server to
-   *  broadcast to other clients that the user has permanently
-   *  leaved the group. Also, the server will remove the client
-   *  from the chatroom
-   *
-   *  @param {Object} data {roomToken, sender}
-   */
-  socket.on('leave room', (data) => {
-    const username = data.sender;
-    const roomToken = data.roomToken;
-    chatRoomQuery.getChatroom(roomToken).then((room) => {
-      return userQuery.leaveChatRoom(username, room._id);
-    }).then((updatedUser) => {
-      socket.to(roomToken).emit('user left', {
-        success: true,
-        username: updatedUser.username,
-        firstName: updatedUser.firstName,
-        ts: updatedUser.updatedAt,
-      });
-      socket.emit('leave room ack', {
-        success: true,
-        ts: updatedUser.updatedAt,
-      });
-      socket.leave(roomToken);
-    }).catch((err) => {
-      console.error('Error while leaving room', err);
-    });
   });
 }
 
--- webapp/src/app/home/join-room/join-room.component.ts
+++ webapp/src/app/home/join-room/join-room.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { APISocketService } from '../../shared/base/api-socket.service';
+
+interface ServerJoinRoomAcknowledgement {
+  success: boolean;
+}
 
 @Component({
   selector: 'app-join-room',
@@ -6,10 +14,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./join-room.component.scss']
 })
 export class JoinRoomComponent implements OnInit {
+  joinRoomForm: FormGroup;
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private socket: APISocketService, private router: Router) { }
 
   ngOnInit() {
+    this.joinRoomForm = this.fb.group({
+      roomToken: this.fb.control('', [Validators.required])
+    });
   }
 
+  onJoinRoom() {
+    this.errorMessage = '';
+    this.joinRoomForm.disable();
+
+    const roomToken: string = this.joinRoomForm.get('roomToken').value;
+    this.socket.getEvent<ServerJoinRoomAcknowledgement>('join room ack')
+      .first()
+      .subscribe(ack => {
+        this.joinRoomForm.enable();
+        if (ack.success) {
+          this.router.navigate([''], { queryParams: { room: roomToken } });
+        } else {
+          this.errorMessage = 'Cannot join room!';
+        }
+      });
+
+    this.socket.sendMessage('join room', { roomToken });
+  }
 }
