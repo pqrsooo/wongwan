6741fb22f73509a634cbf13855fc29a26a0c5d39
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-04-26 02:33:07 +0700

    Add authorization in Socket.io

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const config = require('./config');
 const appServer = require('./app').appServer;
+const appSession = require('./app').appSession;
 const db = require('./db');
 const createSocketServer = require('./socket/socket.base');
 const onConnect = require('./socket/socket.connect').onConnect;
@@ -18,4 +19,16 @@ db.setUpDatabase().then(() => {
 });
 
 const io = createSocketServer(appServer);
+
+io.set('authorization', (handshake, accept) => {
+  appSession(handshake, {}, (err) => {
+    if (err) return accept(err);
+    const session = handshake.session;
+    // check the session is valid
+    // accept(null, session.userid != null);
+    accept(null, true);
+  });
+});
+
 io.on('connection', onConnect);
+
