bffe544443c612929b75ee00b12e560e8a83ef17
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-04-26 02:32:24 +0700

    Remove unwanted key in session

--- api/middleware/unauthorized.js
+++ api/middleware/unauthorized.js
@@ -2,7 +2,6 @@ exports.restrict = (req, res, next) => {
   if (req.session.user) {
     next();
   } else {
-    req.session.error = 'Access denied!';
     res.status(403).json({
       isLogin: false,
       message: req.session.error,
