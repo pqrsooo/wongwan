ac580a6c011e03f2a9291f033f7fb5fb3178e1cf
Parinthorn Saithong <parinthorn.sa@gmail.com>
2017-05-01 20:01:57 +0700

    Add dropdown

--- webapp/src/app/core/top-navigation-bar/top-navigation-bar.component.ts
+++ webapp/src/app/core/top-navigation-bar/top-navigation-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 import { UserService } from '../../shared/user/user.service';
@@ -6,13 +6,17 @@ import { UserService } from '../../shared/user/user.service';
 @Component({
   selector: 'app-top-navigation-bar',
   templateUrl: './top-navigation-bar.component.html',
-  styleUrls: ['./top-navigation-bar.component.scss']
+  styleUrls: ['./top-navigation-bar.component.scss'],
+  host: {
+    '(document:click)': 'hideDropdown($event)',
+  }
 })
 export class TopNavigationBarComponent implements OnInit {
   username$: Observable<string>;
   isLoggedIn$: Observable<boolean>;
+  isDropdownVisible = false;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private _el: ElementRef) { }
 
   ngOnInit() {
     this.username$ = this.userService.getCurrentUser$().map(user => {
@@ -23,5 +27,15 @@ export class TopNavigationBarComponent implements OnInit {
     });
 
     this.isLoggedIn$ = this.userService.isLoggedIn();
+  }
+
+  toggleDropdown() {
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
+
+  hideDropdown(event: any) {
+    if (!this._el.nativeElement.contains(event.target)) {
+      this.isDropdownVisible = false;
+    }
   }
 }
