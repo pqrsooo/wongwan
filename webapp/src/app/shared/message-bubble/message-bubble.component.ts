ab840848376eb7ad2ca1a3b4af4d173879015db6
Kasidit Iamthong <kasidit@outlook.com>
2017-05-01 20:43:16 +0700

    Add username data to app bubble

--- webapp/src/app/shared/message-bubble/message-bubble.component.ts
+++ webapp/src/app/shared/message-bubble/message-bubble.component.ts
@@ -7,6 +7,7 @@ import { Component, HostBinding, Input, OnChanges } from '@angular/core';
 })
 export class MessageBubbleComponent implements OnChanges {
   @Input() name: string | undefined;
+  @Input() username: string | undefined;
   @Input() userId: number | undefined;
   @Input() isOutgoing?: boolean;
   @Input() isBeforeSeparator?: boolean;
